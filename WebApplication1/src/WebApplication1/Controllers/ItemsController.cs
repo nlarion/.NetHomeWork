8dd713739b68b6240ade11ac45502a1337936518
Neil Larion <neil.larion@gmail.com>
2016-04-18 10:46:38 -0700

    Get basic app displaying results from the database

--- WebApplication1/src/WebApplication1/Controllers/ItemsController.cs
+++ WebApplication1/src/WebApplication1/Controllers/ItemsController.cs
@@ -3,17 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Mvc;
+using ToDoApp.Models;
 
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace ToDoApp.Controllers
 {
-    public class HomeController : Controller
+    public class ItemsController : Controller
     {
-        // GET: /<controller>/
+        private ToDoAppContext db = new ToDoAppContext();
         public IActionResult Index()
         {
-            return View();
+            return View(db.Items.ToList());
+        }
+        public IActionResult Details(int id)
+        {
+            var thisItem = db.Items.FirstOrDefault(x => x.ItemId == id);
+            return View(thisItem);
         }
     }
 }
---
+++ WebApplication1/src/WebApplication1/Models/ToDoAppContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Data.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ToDoApp.Models
+{
+    public class ToDoAppContext : DbContext
+    {
+        public virtual DbSet<Item> Items { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=ToDoApp;integrated security = True");
+        }
+    }
+}
