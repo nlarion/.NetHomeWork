65add6fe42e35f22aeaabf5827178a896ad9dc13
Neil Larion <neil.larion@gmail.com>
2016-04-18 11:17:53 -0700

    Build out Create/Update/Delete functionality for ToDo items.

--- WebApplication1/src/WebApplication1/Controllers/ItemsController.cs
+++ WebApplication1/src/WebApplication1/Controllers/ItemsController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Mvc;
 using ToDoApp.Models;
+using Microsoft.Data.Entity;
 
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -12,14 +13,57 @@ namespace ToDoApp.Controllers
     public class ItemsController : Controller
     {
         private ToDoAppContext db = new ToDoAppContext();
+
         public IActionResult Index()
         {
             return View(db.Items.ToList());
         }
+
         public IActionResult Details(int id)
         {
             var thisItem = db.Items.FirstOrDefault(x => x.ItemId == id);
             return View(thisItem);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Item item)
+        {
+            db.Items.Add(item);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public IActionResult Edit(int id)
+        {
+            var thisItem = db.Items.FirstOrDefault(x => x.ItemId == id);
+            return View(thisItem);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Item item)
+        {
+            db.Entry(item).State = EntityState.Modified;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var thisItem = db.Items.FirstOrDefault(x => x.ItemId == id);
+            return View(thisItem);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            var thisItem = db.Items.FirstOrDefault(x => x.ItemId == id);
+            db.Items.Remove(thisItem);
+            db.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
