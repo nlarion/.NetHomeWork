5dd88b405ef73af13dd2eee4618b5ad351b59ca1
Neil Larion <neil.larion@gmail.com>
2016-04-24 11:46:30 -0700

    Add solution to homework

---
+++ WebApplication1/src/WebApplication1/Controllers/ItemsController.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+using ToDoApp.Models;
+using Microsoft.Data.Entity;
+using Microsoft.AspNet.Mvc.Rendering;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ToDoApp.Controllers
+{
+    public class ItemsController : Controller
+    {
+        private ToDoAppContext db = new ToDoAppContext();
+
+        public IActionResult Index()
+        {
+            return View(db.Items.Include(x => x.Category).ToList());
+        }
+
+        public IActionResult Details(int id)
+        {
+            var thisItem = db.Items.FirstOrDefault(x => x.ItemId == id);
+            return View(thisItem);
+        }
+
+        public ActionResult Create()
+        {
+            ViewBag.CategoryId = new SelectList(db.Categories, "CategoryId", "Name");
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Item item)
+        {
+            db.Items.Add(item);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var thisItem = db.Items.FirstOrDefault(d => d.ItemId == id);
+            ViewBag.CategoryId = new SelectList(db.Categories, "CategoryId", "Name");
+            return View(thisItem);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Item item)
+        {
+            db.Entry(item).State = EntityState.Modified;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Delete(int id)
+        {
+            var thisItem = db.Items.FirstOrDefault(x => x.ItemId == id);
+            return View(thisItem);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            var thisItem = db.Items.FirstOrDefault(x => x.ItemId == id);
+            db.Items.Remove(thisItem);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult ToggleDone(int id)
+        {
+            var thisItem = db.Items.FirstOrDefault(x => x.ItemId == id);
+            thisItem.Done = !thisItem.Done;
+            db.Entry(thisItem).State = EntityState.Modified;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+    }
+}
---
+++ WebApplication1/src/WebApplication1/Models/Item.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ToDoApp.Models
+{
+    [Table("Items")]
+    public class Item
+    {
+        [Key]
+        public int ItemId { get; set; }
+        public string Description { get; set; }
+        public bool Done { get; set; }
+        public int CategoryId { get; set; }
+        public virtual Category Category { get; set; }
+    }
+}
